202568d7f6f8091a978bd55621848e30cec029dc
Ziad Abbas <tt933825@gmail.com>
2025-05-03 04:23:03 +0300

    final v

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -7,7 +7,7 @@ const FeatureCard = ({ icon, title, content, index }) => (
   <div
     className={`flex flex-row p-6 rounded-[20px] ${
       index !== features.length - 1 ? "mb-6" : "mb-0"
-    } feature-card hover:scale-105 duration-700 ease-in-out  `}
+    } feature-card`}
   >
     <div
       className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue `}
